f8c974b866c32d7aa9bf7f5d024101c240dd06be
AkashSDas <aakashdas368@gmail.com>
2021-05-28 19:22:49 +0530

    setup GET request in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import Button from "../components/Button";
+import GetOutput from "../components/GetOutput";
+
 function Home() {
+  const [display, setDisplay] = useState(null);
+
+  const btnSections = () => (
+    <section className="btn-section">
+      <Button className="get" text="GET" onClick={() => setDisplay("get")} />
+    </section>
+  );
+
+  const resultSection = () => {
+    if (display === "get") return <GetOutput />;
+    else return null;
+  };
+
   return (
     <main>
       <h1>Axios</h1>
+
+      {btnSections()}
+      {resultSection()}
     </main>
   );
 }
